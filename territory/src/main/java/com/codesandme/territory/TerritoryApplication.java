771d3078325fe7180138ef0a02d11c816f6ee2fb
Leo <leonard.njura@gmail.com>
2023-04-12 13:05:57 +0300

    Setup spring boot repo - create parent maven app - create sample microservice child maven module

---
+++ territory/src/main/java/com/codesandme/territory/TerritoryApplication.java
@@ -0,0 +1,12 @@
+package com.codesandme.territory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerritoryApplication {
+
+	public static void main(String[] args){
+        SpringApplication.run(TerritoryApplication.class, args);
+    }
+}
